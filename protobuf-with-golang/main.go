3ffb7aef5bd7c4b2e889b7d7f217a65ccf75b58e
petrostrak <pit.trak@gmail.com>
2021-02-03 23:26:59 +0200

    Reading and Writing to Disk

--- protobuf-with-golang/main.go
+++ protobuf-with-golang/main.go
@@ -3,13 +3,53 @@ package main
 import (
 	simplepb "Protocol-Buffers/protobuf-with-golang/src/simple"
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/golang/protobuf/proto"
 )
 
 func main() {
-	simple()
+	sm := simple()
+	writeToFile("simple.bin", sm)
+
+	sm2 := &simplepb.SingleMessage{}
+	readFromFile("simple.bin", sm2)
+	fmt.Println(sm2)
 }
 
-func simple() {
+func writeToFile(filename string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatalln("Can't serialize to bytes.", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile(filename, out, 0644); err != nil {
+		log.Fatalln("Can't write to file", err)
+		return err
+	}
+
+	fmt.Println("Data have been written!")
+	return nil
+}
+
+func readFromFile(filename string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Something went wront when reading the file", err)
+		return err
+	}
+
+	if err := proto.Unmarshal(in, pb); err != nil {
+		log.Fatalln("Couldn't put the bytes into the protocol buffers struct.", err)
+		return err
+	}
+
+	return nil
+}
+
+func simple() *simplepb.SingleMessage {
 	sm := simplepb.SingleMessage{
 		Id:         1,
 		Name:       "Petros",
@@ -23,4 +63,6 @@ func simple() {
 	fmt.Println(sm)
 
 	fmt.Println(sm.GetId())
+
+	return &sm
 }
